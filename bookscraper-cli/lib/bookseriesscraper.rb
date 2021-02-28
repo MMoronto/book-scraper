5e3dfb434c028fbbf73eb3d64ff58748922c4c6e
MMoronto <t2wycemo@gmail.com>
2021-02-28 10:09:03 -0500

    Update bookseriesscraper.rb

--- bookscraper-cli/lib/bookseriesscraper.rb
+++ bookscraper-cli/lib/bookseriesscraper.rb
@@ -5,6 +5,16 @@ class BookseriesScraper
     def initialize(series_number)
         @bookseries = Bookseries.new
         @bookseries.series_number = series_number
-        @doc = Nokogiri::HTML(open("https://www.goodreads.com/series/281756-the-expanse-chronological/#{series_number}"))
+        @doc = Nokogiri::HTML(open("https://www.goodreads.com/series/#{series_number}"))
+    end 
+
+    def scrape
+        scrape_details
+        @bookseries #=> This instance should have a listing of books and details
+    end
+
+    def scrape_details
+        # populate @bookseries with more data from the bookseries site
+        release_date = @doc.search("gr-metaText u-inlineBlock")
     end 
 end
