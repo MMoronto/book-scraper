a960e7a574423901528c1f3e7979612b26eab430
MMoronto <t2wycemo@gmail.com>
2021-02-27 20:25:45 -0500

    Update bookseries.rb

--- bookscraper-cli/lib/bookseries.rb
+++ bookscraper-cli/lib/bookseries.rb
@@ -1,9 +1,23 @@
-class Bookseries
-    attr_accessor :books, :series_number, :release_date
+class InvalidType < StandardError; end
 
-    def first_book_url
-        
-        self.books.first.url
+class Bookseries
+    attr_accessor :series_number, :release_date
+    attr_reader :books
+
+    def initialize
+        @books = []
     end
+
+    def books
+        @books.dup.freeze!
+    end
+
+    def add_books(book)
+        if !book.is_a?(Book)
+            raise InvalidType, "must be a Book" 
+        else
+            @books << book
+    end
+
     
-end 
+end
