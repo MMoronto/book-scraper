75b5d0d891409cf36f2fbe1c5ce3ac7995da4016
MMoronto <t2wycemo@gmail.com>
2021-03-03 20:23:53 -0500

    Update bookseriesscraper.rb

--- bookscraper-cli/lib/bookseriesscraper.rb
+++ bookscraper-cli/lib/bookseriesscraper.rb
@@ -5,25 +5,26 @@ class BookseriesScraper
     def initialize(series_number)
         @bookseries = Bookseries.new
         @bookseries.series_number = series_number
-        @doc = Nokogiri::HTML(open("https://www.goodreads.com/series/#{series_number}"))
+        @doc = Nokogiri::HTML(open("https://www.penguinrandomhouse.com/the-read-down/series/#{series_number}"))
     end 
 
     def scrape
         scrape_details
-        @bookseries #=> This instance should have a listing of books and details
+        @bookseries #=> This instance should have a listing of book series and details
     end
 
     def scrape_details
         # populate @bookseries with more data from the bookseries site
-        @bookseries.release_date = @doc.search("gr-metaText.u-inlineBlock").text.gsub("series #{@series_number} - ", "").strip
+        @bookseries.boxset_price = @doc.search("div.buy-price").text.gsub("series #{@series_number} - ", "").strip
     end
 
-    def scrape_books
-        # I would break the convention of only knowing about the Bookseries and let it create books
-        @doc.search("listWithDividers").each do |book_table|
-            # instantiate the book
-            # scrape the data
-            # add the book to the bookseries
-            
-    end
+    # def scrape_books
+    #     # I would break the convention of only knowing about the Bookseries and let it create books
+    #     @doc.search("listWithDividers").each do |book_table|
+    #         # instantiate the book
+    #         b = Book.new
+    #         # scrape the data
+    #         binding.pry
+    #         # add the book to the bookseries
+    # end
 end
